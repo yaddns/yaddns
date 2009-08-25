9a47e655a40e32f4b413ed175b857f9c4b08d69d
homer242 <avd@patatrac.info>
2009-08-25 09:18:09 +0000

    * fix current wan ip used
    git-svn-id: svn://patatrac.info/yaddns/trunk@42 62bb416e-a5ad-dd11-9688-001cc03c355a

--- src/ctl.c
+++ src/ctl.c
@@ -358,7 +358,7 @@ void ctl_preselect(struct cfg *cfg)
         
         
         /* transform wan ip raw in ascii char */
-        if(!inet_ntop(AF_INET, &wanip, buf_wanip, sizeof(buf_wanip)))
+        if(!inet_ntop(AF_INET, &curr_wanip, buf_wanip, sizeof(buf_wanip)))
         {
                 log_error("inet_ntop(): %m");
                 return;
