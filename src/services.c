c745414507bfa4d1895b64f645204aaac21b3857
raphaelh <rhk@cksum.org>
2010-12-08 16:10:52 +0000

    add support for ovh
    git-svn-id: svn://patatrac.info/yaddns/trunk@96 62bb416e-a5ad-dd11-9688-001cc03c355a

--- src/services.c
+++ src/services.c
@@ -5,10 +5,12 @@
 
 extern struct service dyndns_service;
 extern struct service noip_service;
+extern struct service ovh_service;
 
 void services_populate_list(void)
 {
-     INIT_LIST_HEAD(&service_list);
-     list_add_tail( &dyndns_service.list, &service_list );
-     list_add_tail( &noip_service.list, &service_list );
+	INIT_LIST_HEAD(&service_list);
+	list_add_tail( &dyndns_service.list, &service_list );
+	list_add_tail( &noip_service.list, &service_list );
+	list_add_tail( &ovh_service.list, &service_list );
 }
