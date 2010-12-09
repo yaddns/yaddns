527ad2b3522452411c0f7117334d0ff4e3079d86
raphaelh <rhk@cksum.org>
2010-12-09 15:22:01 +0000

    add support for dyndns.it
    git-svn-id: svn://patatrac.info/yaddns/trunk@98 62bb416e-a5ad-dd11-9688-001cc03c355a

--- src/services.c
+++ src/services.c
@@ -5,6 +5,7 @@
 
 extern struct service changeip_service;
 extern struct service dyndns_service;
+extern struct service dyndnsit_service;
 extern struct service noip_service;
 extern struct service ovh_service;
 
@@ -13,6 +14,7 @@ void services_populate_list(void)
 	INIT_LIST_HEAD(&service_list);
 	list_add_tail( &changeip_service.list, &service_list );
 	list_add_tail( &dyndns_service.list, &service_list );
+	list_add_tail( &dyndnsit_service.list, &service_list );
 	list_add_tail( &noip_service.list, &service_list );
 	list_add_tail( &ovh_service.list, &service_list );
 }
