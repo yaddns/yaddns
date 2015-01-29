c2aea0d72edde53fd0a94c02d05cbde84b1c21b3
Dmitriy Bogdanov <di72nn@gmail.com>
2015-01-29 21:46:14 +0400

    Add support for duckdns.org service

--- src/services.c
+++ src/services.c
@@ -9,6 +9,7 @@ extern struct service dyndnsit_service;
 extern struct service noip_service;
 extern struct service ovh_service;
 extern struct service sitelutions_service;
+extern struct service duckdns_service;
 
 void services_populate_list(void)
 {
@@ -19,4 +20,5 @@ void services_populate_list(void)
 	list_add_tail( &noip_service.list, &service_list );
 	list_add_tail( &ovh_service.list, &service_list );
 	list_add_tail( &sitelutions_service.list, &service_list );
+        list_add_tail( &duckdns_service.list, &service_list );
 }
